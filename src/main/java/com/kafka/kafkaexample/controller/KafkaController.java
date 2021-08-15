54f60d3d07c5ec3c171cb45da6a58fc0e946d6cc
Siva kumar <shivku.goku@gmail.com>
2021-08-15 15:37:26 +0530

    Not working

---
+++ src/main/java/com/kafka/kafkaexample/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.kafka.kafkaexample.controller;
+
+import com.kafka.kafkaexample.consumer.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestBody String message) {
+        this.producer.sendMessage(message);
+    }
+}
+
